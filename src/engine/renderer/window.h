eefb4c03147a801758d964eb8e16c4cefb7952e0
Merli Mejia <merli@hostop.ai>
2026-08-09 10:32:40 -0400

    feat: 🎸 Renderer Window

---
+++ src/engine/renderer/window.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <GLFW/glfw3.h>
+
+#include <functional>
+#include <iostream>
+#include <string.h>
+
+namespace Renderer {
+
+struct Window {
+  GLFWwindow *window = nullptr;
+
+  void init(const uint32_t width, const uint32_t heigth, std::string name) {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(width, heigth, name.c_str(), nullptr, nullptr);
+  }
+
+  void update(std::function<void()> callback) {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+      callback();
+    }
+  }
+
+  void cleanup() {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
+};
+
+} // namespace Renderer
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,51 @@
+#include <memory>
+#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
+#include <GLFW/glfw3.h>
+
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+
+#include "engine/renderer/window.h"
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+Renderer::Window window;
+
+class HelloTriangleApplication {
+public:
+  void run() {
+    window.init(WIDTH, HEIGHT, "Renderer");
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  void initVulkan() {}
+
+  void mainLoop() {
+    window.update([]() {
+
+    });
+  }
+
+  void cleanup() {}
+};
 
 int main() {
+  try {
+    HelloTriangleApplication app;
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  std::cout << "A new renderer!" << std::endl;
-
-  return 0;
+  return EXIT_SUCCESS;
 }
